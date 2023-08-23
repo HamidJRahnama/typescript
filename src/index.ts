5537b7335ab37d9c9593662e725beb02b1950f14
Hamid.J.Rahnama <hamidrahnamapc@gmail.com>
2023-08-23 20:33:24 +0330

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,62 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+let num: number = 10;
+let str: string = "hamid";
+let bool: boolean = true;
 
-import confetti from 'canvas-confetti';
+str = "ali";
+num = 87;
+console.log("basic ", str, num, bool);
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// #############################
+console.log("");
+
+enum direction {
+  up = 10,
+  down = 20,
+  laft = 30,
+  right = 40,
+}
+
+console.log("enum", direction);
+
+// ##############################
+console.log("");
+
+function sum(x: number, y: number): number {
+  return x + y;
+}
+console.log("sum", sum(1, 3));
+
+let sum1 = (x: number, y: number): number => {
+  return x + y;
+};
+console.log("sum1", 3 + 5);
+
+// this is NOT funtion
+let sum2: (x: number, y: number) => number;
+sum2 = function (x, y) {
+  return x + y;
+};
+console.log("sum2", sum2(7, 9));
+
+// also u CAN right it like this
+let sum3: (x: number, y: number) => number = (x, y) => x + y;
+console.log("sum3", sum3(11, 13));
+
+let sum4 = (x: number, y: number = 2): number => {
+  return x + y;
+};
+console.log("sum4: Defult Value", sum4(1, 5));
+
+let sum5 = (x?: number, y?: number): number => {
+  if (x && y) {
+    return x + y;
+  } else {
+    return -0;
+  }
+};
+console.log("sum5: optional", sum5());
+
+// ##############################
+console.log("");
+
+console.log("");
