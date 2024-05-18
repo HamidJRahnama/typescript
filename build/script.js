9beaa930d27fb0c2001fac4d6540d2a19fc25448
Hamid.J.Rahnama <hamidrahnamapc@gmail.com>
2024-05-18 18:24:37 +0330

    init

--- build/script.js
+++ build/script.js
@@ -9,4 +9,4 @@ user2[6] = 75;
 let user3 = ["Hamid", 20, false];
 user3[0] = "ali";
 user3[2] = 30; // Error : Type 'number' is not assignable to type 'boolean'.
-user3[5] = "demo"; 
+user3[5] = "demo"; // Error : Tuple type '[string, number, boolean]' of length '3' has no element at index '5'
